2b34fe608e84627cbe6ce2a7d023b2157e7c69b0
Vinchx <nzxtvinix@gmail.com>
2025-03-05 12:02:03 +0700

    halo

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,18 @@
 import Lanyard from "@/components/Lanyard/Lanyard";
 import ScrollVelocity from "@/components/ScrollVelocity/ScrollVelocity";
 
-
 export default function Home() {
   return (
     <div>
-      <div className="h-screen w-full">
-
-      </div>
       <div className="flex items-center relative h-screen w-full overflow-hidden">
         <div className="absolute z-0 top-[340px]">
-        <ScrollVelocity />
+          <ScrollVelocity />
         </div>
         <div className="absolute w-full h-full z-1">
-        <Lanyard />
+          <Lanyard />
         </div>
       </div>
-      
+      <div className="h-screen w-full"></div>
     </div>
   );
 }
