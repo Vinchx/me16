8661e991532f365944b5088a8ca32bc7795e32a7
Vinix <nzxtvinix@gmail.com>
2025-04-15 22:45:32 +0700

    mencoba fix

--- src/app/home/HomeDesktop1200.jsx
+++
@@ -1,4 +0,0 @@
-// app/home/HomeCompact.jsx
-export default function HomeCompact() {
-  return <div className="text-center text-sm text-red-500">Compact View</div>;
-}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,45 +1,173 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 
-// Lazy load komponen
-const HomeMobile = dynamic(() => import("@/app/home/HomeMobile"), {
+const Lanyard = dynamic(() => import("@/components/Lanyard/Lanyard"), {
   ssr: false,
 });
-const HomeDesktop1080 = dynamic(() => import("@/app/home/HomeDesktop1080"), {
+const ScrollVelocity = dynamic(
+  () => import("@/components/ScrollVelocity/ScrollVelocity"),
+  { ssr: false }
+);
+const ScrollVelocity2 = dynamic(
+  () => import("@/components/ScrollVelocity/ScrollVelocity2"),
+  { ssr: false }
+);
+const ScrollVelocity3 = dynamic(
+  () => import("@/components/ScrollVelocity/ScrollVelocity3"),
+  { ssr: false }
+);
+const Particles = dynamic(() => import("@/components/Particles/Particles"), {
   ssr: false,
 });
-const HomeDesktop1200 = dynamic(() => import("@/app/home/HomeDesktop1200"), {
+const AnimatedText = dynamic(
+  () => import("@/components/AnimatedText/AnimatedText"),
+  { ssr: false }
+);
+const BlurText = dynamic(() => import("@/components/BlurText/BlurText"), {
+  ssr: false,
+});
+const GitHubCard = dynamic(() => import("@/components/GitHubCard/GitHubCard"), {
+  ssr: false,
+});
+const Carousel = dynamic(() => import("@/components/Carousel/Carousel"), {
+  ssr: false,
+});
+const PixelTransition = dynamic(
+  () => import("@/components/PixelTransition/PixelTransition"),
+  { ssr: false }
+);
+
+const TrueFocus = dynamic(() => import("@/components/TrueFocus/TrueFocus"), {
   ssr: false,
 });
 
-export default function Page() {
-  const [screenType, setScreenType] = useState("normal");
+import Image from "next/image";
 
-  useEffect(() => {
-    const checkScreen = () => {
-      const height = window.innerHeight;
-
-      if (height > 1200) {
-        setScreenType("tall");
-      } else if (height < 1080) {
-        setScreenType("compact");
-      } else {
-        setScreenType("normal");
-      }
-    };
-
-    checkScreen();
-    window.addEventListener("resize", checkScreen);
-    return () => window.removeEventListener("resize", checkScreen);
-  }, []);
-
+export default function Home() {
   return (
-    <>
-      {screenType === "compact" && <HomeMobile />}
-      {screenType === "normal" && <HomeDesktop1080 />}
-      {screenType === "tall" && <HomeDesktop1200 />}
-    </>
+    /* Div utama */
+    <div className="relative">
+      {/* Particles sebagai background */}
+      <div className="absolute w-full min-h-screen -z-40 bg-[#0D1821]">
+        {/* <Particles moveParticlesOnHover={true} /> */}
+      </div>
+
+      {/* scroll velocity */}
+      <div className="absolute left-[8rem] pb-3 bg-[#B4CDED] text-[#0D1821] -translate-y-1/2 rotate-90 origin-left z-20 w-[410vh] ">
+        <ScrollVelocity2 />
+      </div>
+      <div className="absolute right-[8rem] pb-3 bg-[#B4CDED] text-[#0D1821] -translate-y-1/2 -rotate-90 origin-right z-20 w-[410vh]">
+        <ScrollVelocity3 />
+      </div>
+
+      {/* Halaman pertama */}
+      <div className="relative z-10 min-h-screen flex flex-col items-center justify-center">
+        {/* Animasi text dan foto */}
+        <div className="mt-10 mb-5 flex md:flex-row items-center gap-x-[10rem] bg-[#344966] px-10 py-6  rounded-3xl cursor-crosshair">
+          {/* Animasi text */}
+          <div className="text-[4.5em] text-center md:text-left">
+            <AnimatedText />
+          </div>
+
+          {/* Foto */}
+          <div className="">
+            <PixelTransition
+              firstContent={
+                <Image
+                  src="/assets/mejir.png"
+                  alt="atmin"
+                  width={500}
+                  height={500}
+                  className="w-full h-full object-cover"
+                />
+              }
+              secondContent={
+                <div className="w-full h-full grid place-items-center bg-[#0D1821] ">
+                  <Image
+                    src="/assets/New Project 5 ;[D0995BB].gif"
+                    alt="atmin"
+                    width={500}
+                    height={500}
+                    className="w-full h-full object-cover z-20"
+                  />
+
+                  <p className="absolute font-black text-5xl text-white z-50">
+                    Meow!
+                  </p>
+                </div>
+              }
+              gridSize={3}
+              pixelColor="#ffffff"
+              animationStepDuration={0.4}
+              className="custom-pixel-card"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Halaman kedua */}
+      <div className="relative z-10 bg-[#0D1821] rounded-b-[6rem] [box-shadow:0_2rem_0_2rem_#BFCC94]">
+        <div>
+          <TrueFocus
+            sentence="ME!! 🗿😆"
+            manualMode={true}
+            blurAmount={10}
+            borderColor="#BFCC94"
+            animationDuration={1}
+            pauseBetweenAnimations={1}
+          />
+
+          <BlurText
+            text="Hi, I’m Bagus Prasetya Ramadhan, a student with a passion for web development & creative coding. Even though I’m still in school, I love experimenting with React, Next.js, and UI animations, turning ideas into real projects. I enjoy solving problems, designing interactive experiences, and constantly learning new things. Instead of waiting for the “perfect time” to start, I believe in diving in, building, breaking, and improving—because that’s how real learning happens! 🚀. Right now, I’m exploring Laravel to expand my backend development skills, with the goal of becoming a Fullstack Developer. I’m also working on a simple chatbot website using Next.js and LM Studio API, pushing my knowledge of AI integration in web apps. Besides coding, I love exploring technology trends, playing games, and discovering new tools like Tailwind CSS to enhance my workflow. I’m always open to connecting, collaborating, or just geeking out over cool tech stuff, so feel free to reach out! 😃"
+            delay={0}
+            animateBy="words"
+            direction="bottom"
+            className="text-lg mx-60 text-justify leading-loose tracking-wide text-foreground"
+          />
+        </div>
+        <div className="flex items-center justify-evenly mt-10">
+          <GitHubCard />
+          <div className="py-10">
+            <Carousel
+              baseWidth={350}
+              autoplay={true}
+              autoplayDelay={3000}
+              pauseOnHover={true}
+              loop={true}
+              round={false}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Halaman ketiga */}
+      <div className="absolute top-[216.5vh] min-h-screen z-30 min-w-full grid grid-cols-3 items-center justify-center rounded-b-[6rem] bg-[#f0f3ff]">
+        <div>
+          <iframe
+            allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+            frameborder="0"
+            height="450"
+            className="w-full max-w-max-[660px] overflow-hidden rounded-[10px] px-5"
+            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+            src="https://embed.music.apple.com/id/playlist/citys/pl.u-d2b05dXtLREWaVx"
+          ></iframe>
+        </div>
+        <div className="w-full">
+          <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
+        </div>
+        <iframe
+          allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+          frameborder="0"
+          height="450"
+          className="w-full max-w-max-[660px] overflow-hidden rounded-[10px]  px-5"
+          sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+          src="https://embed.music.apple.com/id/playlist/wests/pl.u-11zBJ73cNJmG6d4"
+        ></iframe>
+      </div>
+      <div className="absolute z-10 bg-[#BFCC94] min-h-screen min-w-full top-[312vh] flex flex-col items-center justify-center">
+        <div></div>
+      </div>
+    </div>
   );
 }
