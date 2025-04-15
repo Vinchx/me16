23a9a50b17ec374f569d02edbc85e365923d34e9
Vinix <nzxtvinix@gmail.com>
2025-04-15 23:57:43 +0700

    1

--- src/app/Home/MobilePage.jsx
+++ src/app/Home/MobilePage.jsx
@@ -1,4 +1,12 @@
 // MobilePage.jsx
 export default function MobilePage() {
-  return <div className="bg-red-600 p-4">Ini tampilan Mobile 📱</div>;
+  return (
+    <div className="bg-red-600 p-4">
+      Ini tampilan Mobile 📱
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
