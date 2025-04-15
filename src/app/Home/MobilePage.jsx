2f76ab7ac822bb8fd013fc57f332a4bfb398826c
Vinix <nzxtvinix@gmail.com>
2025-04-15 23:59:25 +0700

    3

--- src/app/Home/MobilePage.jsx
+++ src/app/Home/MobilePage.jsx
@@ -8,9 +8,7 @@ export default function MobilePage() {
       <div></div>
       <div></div>
       <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div></div>4
     </div>
   );
 }
